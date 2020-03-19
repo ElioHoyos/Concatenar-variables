d9b60f1c9a2113cdc808025662b0dea83fe04d9c
Elio Hoyos <elio18david@gmail.com>
2020-03-19 15:31:06 -0500

    concatenar variable var

---
+++ src/main/java/elio/com/ejerciciovariable2/TipoVariable.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elio.com.ejerciciovariable2;
+
+/**
+ *
+ * @author FRANKOYCI
+ */
+public class TipoVariable {
+    public static void main(String[] args) {
+        //concatenar variables var
+        var usuario = "Elio";
+        var saludar = "Hola";
+        
+        System.out.println(saludar + usuario);
+        
+    }
+}
